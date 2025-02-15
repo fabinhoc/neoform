eabbbe6661309c33953b89f137afa1e7c4f3c5e3
Fabio Cruz <fabinhocosta1202@gmail.com>
2025-02-15 12:09:51 -0300

    feat: Add Soft delete in Request manager object.

--- app/Models/RequestManager.php
+++ app/Models/RequestManager.php
@@ -4,9 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class RequestManager extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'name',
         'description',
@@ -16,5 +20,10 @@ class RequestManager extends Model
     public function company(): BelongsTo
     {
         return $this->belongsTo(Company::class);
+    }
+
+    public function eforms(): HasMany
+    {
+        return $this->hasMany(EForm::class);
     }
 }
